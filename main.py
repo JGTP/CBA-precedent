4347db64759e4fb23d42981ac6de66dc49e0d566
JGTP <j.g.t.peters@uu.nl>
2022-06-23 15:43:02 +0200

    the problem is diff

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from experiments import authoritativeness, landmarks
 
 if __name__ == "__main__":
     # landmarks.experiment()
+    # authoritativeness.experiment(
+    #     csvs=[
+    #         # "data/mushroom.csv",
+    #         "data/churn.csv",
+    #         # "data/admission.csv",
+    #     ],
+    #     m="pearson",
+    #     make_consistent=True,
+    # )
     authoritativeness.experiment(
         csvs=[
-            "data/mushroom500.csv",
-            "data/churn500.csv",
-            "data/admission.csv",
-        ],
-        m="pearson",
-        make_consistent=True,
-    )
-    authoritativeness.experiment(
-        csvs=[
-            "data/mushroom500.csv",
-            "data/churn500.csv",
-            "data/admission.csv",
+            # "data/mushroom.csv",
+            "data/churn.csv",
+            # "data/admission.csv",
         ],
         m="pearson",
         make_consistent=False,
