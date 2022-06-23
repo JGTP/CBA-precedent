4a2212766ed05764aebc5f273fa0e5e7d16c4d8c
JGTP <j.g.t.peters@uu.nl>
2022-06-23 21:30:38 +0200

    that should be it

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from experiments import authoritativeness, landmarks
 
 if __name__ == "__main__":
     # landmarks.experiment()
-    # authoritativeness.experiment(
-    #     csvs=[
-    #         "data/mushroom.csv",
-    #         "data/churn.csv",
-    #         "data/admission.csv",
-    #     ],
-    #     m="pearson",
-    #     make_consistent=True,
-    # )
+    authoritativeness.experiment(
+        csvs=[
+            "data/mushroom.csv",
+            "data/churn.csv",
+            "data/admission.csv",
+        ],
+        m="pearson",
+        make_consistent=True,
+    )
     authoritativeness.experiment(
         csvs=[
             "data/mushroom.csv",
--- tests/test_best_precedents.py
+++ tests/test_best_precedents.py
@@ -7,11 +7,11 @@ from precedents import get_best_precedents
 @pytest.fixture()
 def inconst_csv(tmp_path_factory):
     data = {
-        "Gift": [1, 1, 1, 1, 1, 1, 1, 1],
-        "Present": [1, 1, 1, 1, 1, 1, 1, 1],
-        "Website": [0, 0, 0, 2, 2, 2, 15, 15],
-        "High-cost": [0, 0, 0, 0, 0, 0, 0, 0],
-        "Label": [1, 1, 1, 1, 1, 0, 1, 0],
+        "Gift": [1, 1, 1, 1, 1, 1],
+        "Present": [1, 1, 1, 1, 1, 1],
+        "Website": [0, 0, 0, 2, 2, 2],
+        "High-cost": [0, 0, 0, 0, 0, 0],
+        "Label": [1, 0, 1, 1, 1, 1],
     }
     df = pd.DataFrame(data)
     path = tmp_path_factory.mktemp("data") / "inconst.csv"
@@ -23,21 +23,24 @@ def test_find_best_precedents_naive(inconst_csv):
     CB = CaseBase(inconst_csv)
     case = CB[0]
     precedents = get_best_precedents(case, CB)
-    assert len(precedents) == 5
-    assert precedents[0][0] == 1
-    assert precedents[1][0] == 2
-    assert precedents[2][0] == 3
-    assert precedents[3][0] == 4
-    assert precedents[4][0] == 6
+    assert len(precedents) == 1
+    assert precedents[0][0] == 2
 
 
 def test_find_best_precedents_rel(inconst_csv):
     CB = CaseBase(inconst_csv, auth_method="relative")
     case = CB[0]
     precedents = get_best_precedents(case, CB)
-    assert len(precedents) == 5
-    assert precedents[0][0] == 1
-    assert precedents[1][0] == 2
-    assert precedents[2][0] == 3
-    assert precedents[3][0] == 4
-    assert precedents[4][0] == 6
+    assert len(precedents) == 4
+    assert precedents[0][0] == 2
+    assert precedents[1][0] == 3
+    assert precedents[2][0] == 4
+    assert precedents[3][0] == 5
+
+
+def test_find_best_precedents_abs(inconst_csv):
+    CB = CaseBase(inconst_csv, auth_method="absolute")
+    case = CB[0]
+    precedents = get_best_precedents(case, CB)
+    assert len(precedents) == 1
+    assert precedents[0][0] == 2
